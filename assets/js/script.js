8184a26d46afd04652487c3f060e72700e147b22
Kunalsonar007 <kunalwankhede958@gmail.com>
2023-10-07 20:28:01 +0530

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+/**
+ * Countdown for the launch date
+ */
+
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 10, 2023 23:59:59").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function () {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the remaining time
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the respective elements
+  document.querySelector('.countdown .time[aria-label="days"]').textContent = days;
+  document.querySelector('.countdown .time[aria-label="hours"]').textContent = hours;
+  document.querySelector('.countdown .time[aria-label="minutes"]').textContent = minutes;
+  document.querySelector('.countdown .time[aria-label="seconds"]').textContent = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector('.countdown').innerHTML = "GRAB NOW!";
+  }
+}, 1000);
+
+
+/**
+ * navbar toggle
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header sticky & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
